ff08ee6e00988bb5bb4df8823d92a0754350af2b
jbjanda <jbjanda@gmail.com>
2017-09-18 20:48:10 -0400

    Added delete file

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -25,6 +25,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db)=>{
 db.collection('Users').find({name: 'Mike'}).toArray().then((docs)=>{
     // console.log(`Number of Users ${count}`);
     console.log(JSON.stringify(docs,undefined,2));
+    console.log(`${docs}`, docs[0]._id);
 }, (err)=>{
     console.log('unable to find data in todos', err);
 });
